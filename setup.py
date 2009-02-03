462e3064aa9085ab4246dc1a5de4224e00cbf676
aran.donohue <aran.donohue@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-03 18:06:50 +0000

    new review: owner: veronica.quinones cc: jobias.red labels: ReadyForReview, Priority-Medium summary: Review build changes. branch: /branches/ad/with-setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-
-setup(name='PyVCAL',
-	  version='0.01',
-	  description='Python Version Control Abstraction Layer',
-	  author='The PyVCAL Team',
-	  author_email='pysync-discuss@googlegroups.com',
-	  url='http://code.google.com/p/pysync',
-	  packages=['pyvcal', 'pyvcal.util'],
-	  package_dir={'pyvcal': 'src/pyvcal'})
+setup(
+   name='PyVCAL',
+   version='0.01',
+   description='Python Version Control Abstraction Layer',
+   long_description="""\
+   PyVCAL is a version control system abstraction layer.
+	    
+   Modern version control systems such as Git, Perforce and Subversion provide APIs. These APIs share many concepts but it is not easy to write code to support more than one API.
+	    
+   PyVCAL aims to provide a common interface to these APIs. It is analogous to ODBC or JDBC for databases.
+   """,
+   author='The PyVCAL Team',
+   author_email='pysync-discuss@googlegroups.com',
+   maintainer='The PyVCAL Team',
+   maintainer_email='pysync-discuss@googlegroups.com',
+   url='http://code.google.com/p/pysync',
+   packages=find_packages(),
+   classifiers=[ 
+   'Development Status :: 1 - Planning',
+   'Intended Audience :: Developers',
+   'Intended Audience :: System Administrators',
+   'License :: OSI Approved :: BSD License',
+   'Natural Language :: English',
+   'Programming Language :: Python',
+   'Topic :: Software Development :: Libraries',
+   'Topic :: Software Development :: Version Control'
+   ]
+   #	    ,platforms=""""""
+)
