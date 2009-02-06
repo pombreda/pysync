0e48bf05438c18ecd3dcfaa9442ea8269890c0e2
aran.donohue <aran.donohue@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-06 21:41:27 +0000

    new review: owner: christian.muise labels: ReadyForReview summary: Changes to interface to support Revision#get_diff_with_parent branch: trunk/

--- pyvcal/revision.py
+++ pyvcal/revision.py
@@ -11,7 +11,14 @@ class Revision(object):
         """Get the RevisionProperties for this revision."""
         raise NotImplementedError
         
+    def get_diff_with_parent(paths=None):
+        """Return the RevisionDiff from this revision to its parent, optionally restricted to the given file(s) on paths
+        
+        If there is more than one parent, this method may return a fake RevisionDiff with no content to represent a merge.
+        """
+        raise NotImplementedError
+        
     @classmethod
     def get_diff(cls, a, b, paths=None):
-        """Return the diff from Revision a to Revision b, optionally restricted to the given file(s) on paths"""
+        """Return the RevisionDiff from Revision a to Revision b, optionally restricted to the given file(s) on paths"""
         raise NotImplementedError 
--- pyvcal/revisiondiff.py
+++ pyvcal/revisiondiff.py
@@ -2,4 +2,9 @@ class RevisionDiff(object):
     """The set of changes needed to transform one revision into another."""
     def __init__(self):
         super(RevisionDiff, self).__init__()
-        
+
+class NullRevisionDiff(RevisionDiff):
+    """A fake diff for when a revision doesn't have a unique parent."""
+    def __init__(self):
+        super(NullRevisionDiff, self).__init__()
+
