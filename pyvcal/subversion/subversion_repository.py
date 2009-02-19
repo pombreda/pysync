e582ce0adb913049eaaccd4965908cf00212063f
veronica.quinones <veronica.quinones@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-19 20:58:02 +0000

    initial code for connecting to the repo with svn

---
+++ pyvcal/subversion/subversion_repository.py
@@ -0,0 +1,72 @@
+from pyvcal.subversion import subversion_branch as ibranch
+import os
+from datetime import datetime
+from subvertpy import repos, ra, NODE_NONE, NODE_DIR, NODE_FILE
+
+class Repository():
+
+	def __init__(self, path=None):
+		""" Constructor for the SVN Repository object """
+		self.path = path
+		#super(Repository, self).__init__()
+
+	def get_uri(self):
+        	""" Return the URI of the repository """
+		return self.path
+	
+	def connect(self, path):
+		""" Given a path, then connect to that path """
+		self.path = path
+		self.ra_api = ra.RemoteAccess(path)
+
+	def get_revision(self):
+		""" Return a dictionary of Revision objects where the key is the revision_id 
+		and the value is the actual Revision object """
+		rev_id = self.ra_api.get_latest_revnum()
+		self._log(rev=rev_id)
+	
+	def get_branches(self):
+		""" Return the branches available in the repository """
+	        # assume: /trunk, /branches
+	        branch_dict = {}
+
+	        # The first element in the top level info is the list of files
+		file_list = _file_list('', self.ra_api)
+	        if 'trunk' in file_list:
+		        branch_dict['trunk'] = ibranch('trunk', self.ra_api)
+
+		if 'branches' in file_list:
+			branch_list = _file_list('branches', self.ra_api)
+			for branch in branch_list:
+				branch_dict[branch] = ibranch(('branches/' + branch), self.ra_api)
+		return branch_dict
+	
+	""" Helper methods for this class """
+	def _log(self, path='', rev=None):
+		""" Return a Revision object scoped to path with revision id as rev """
+		rev = rev or self.ra_api.get_latest_revnum()
+	    	log_path = path # may need to join it with the current branch's path?
+		
+		# dictionary containing the revisions of the repository as key-value pairs, where the key is the revision_id 
+		# and the value would be a Revision object.
+		revision_dict = {}
+
+	        def cb(paths, revnum, props, has_children=False):
+				paths = paths or {}
+				revision_dict[revnum] = Revision(revnum,
+                                       props.get('svn:author', ''),
+                                       props.get('svn:log', ''),
+                                       datetime.strptime(
+                                               props['svn:date'].split('.')[0],
+                                               "%Y-%m-%dT%H:%M:%S"),
+                                       paths.keys(),
+                                       self.ra_api,
+                                       log_path)
+
+	        self.ra_api.get_log(callback=cb, paths=[log_path],
+                            start=0, end=rev,
+                            discover_changed_paths=True,
+                            revprops=["svn:date", "svn:author", "svn:log"])
+
+        	return revision_dict
+
