f2d92c4fdf4aa1e52c7d33d860e9c1515cf94a13
aran.donohue <aran.donohue@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-20 20:06:16 +0000

    Import interface for __init__ files.

--- pyvcal/__init__.py
+++ pyvcal/__init__.py
@@ -1,3 +1,28 @@
+"""
+This module represents an interface to a version control system.
+Except get_api, the code is not intended to be run---it is documentation in 
+Python syntax.
+
+For usage information for get_api(), see USAGE_STRING.
+
+Version control system api implementors should copy the following import 
+statements which allow a user of get_api() to use e.g. 
+
+get_api(...).Repository
+
+"""
+
+from branch import Branch
+from file import File
+from filediff import FileDiff
+from path import Path
+from repository import Repository
+from revision import Revision
+from revisiondiff import RevisionDiff
+from revisionproperties import RevisionProperties
+from tree import Tree
+from user import User
+
 import perforce, subversion, git_wrapper as git
 
 USAGE_STRING = """
