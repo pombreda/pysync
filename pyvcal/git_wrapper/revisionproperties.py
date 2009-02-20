f252606041fefc0c306482dd299bae2f85a5f510
christian.muise <christian.muise@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-20 20:04:31 +0000

    Added revisionproperties.py for git.

---
+++ pyvcal/git_wrapper/revisionproperties.py
@@ -0,0 +1,30 @@
+class RevisionProperties(object):
+    """Metadata for a revision"""
+    def __init__(self):
+        super(RevisionProperties, self).__init__()
+    
+    def get_revision(self):
+        """Return the revision to which these properties apply."""
+        pass
+        
+    def get_commit_message(self):
+        """Return a unicode string containing the commit message for the Revision."""
+        pass
+        
+    def get_committer(self):
+        """Get the User who committed the Revision"""
+        pass
+        
+    def get_time(self):
+        """Return the (TODO dataformat) of the revision"""
+        pass
+    
+    def get_revision_id(self):
+        """Return the revision identifier of the revision."""
+        pass
+    
+    revision = property(get_revision)
+    commit_message = property(get_commit_message)
+    committer = property(get_committer)
+    time = property(get_time)
+    revision_id = property(get_revision_id)
