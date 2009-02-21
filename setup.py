c40043c19bd3e9550d88a0d2da06bfce7e90e6e4
aran.donohue <aran.donohue@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-21 17:02:12 +0000

    Add sub-library dependencies to setup.py as optional features.

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
 from setuptools import setup, find_packages
 
 setup(
-   name='PyVCAL',
-   version='0.01',
-   description='Python Version Control Abstraction Layer',
-   long_description="""\
-   PyVCAL is a version control system abstraction layer.
-	    
-   Modern version control systems such as Git, Perforce and Subversion provide APIs. These APIs share many concepts but it is not easy to write code to support more than one API.
-	    
-   PyVCAL aims to provide a common interface to these APIs. It is analogous to ODBC or JDBC for databases.
-   """,
-   author='The PyVCAL Team',
-   author_email='pysync-discuss@googlegroups.com',
-   maintainer='The PyVCAL Team',
-   maintainer_email='pysync-discuss@googlegroups.com',
-   url='http://code.google.com/p/pysync',
-   packages=find_packages(),
-   classifiers=[ 
-   'Development Status :: 1 - Planning',
-   'Intended Audience :: Developers',
-   'Intended Audience :: System Administrators',
-   'License :: OSI Approved :: BSD License',
-   'Natural Language :: English',
-   'Programming Language :: Python',
-   'Topic :: Software Development :: Libraries',
-   'Topic :: Software Development :: Version Control'
-   ],
-   test_suite="pyvcal.tests.test_all"
-   #	    ,platforms=""""""
+    name='PyVCAL',
+    version='0.01',
+    description='Python Version Control Abstraction Layer',
+    long_description="""\
+    PyVCAL is a version control system abstraction layer.
+    
+    Modern version control systems such as Git, Perforce and Subversion provide APIs. These APIs share many concepts but it is not easy to write code to support more than one API.
+    
+    PyVCAL aims to provide a common interface to these APIs. It is analogous to ODBC or JDBC for databases.
+    """,
+    author='The PyVCAL Team',
+    author_email='pysync-discuss@googlegroups.com',
+    maintainer='The PyVCAL Team',
+    maintainer_email='pysync-discuss@googlegroups.com',
+    url='http://code.google.com/p/pysync',
+    packages=find_packages(),
+    classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Version Control'
+    ],
+    test_suite="pyvcal.tests.test_all",
+    extras_require = {
+        'git' : ["GitPython == 0.1.6"],
+        'subversion' : ["subvertpy == 0.6.4"],
+        'perforce' : ["P4Python == 2008.2"],
+    }
+    #	    ,platforms=""""""
 )
