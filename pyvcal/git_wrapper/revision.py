d03e62037ee8e2695e5ad983f0d648798b087abc
jobias.red <jobias.red@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-02-21 16:50:27 +0000

    git_wrapper/revisionproperties.py done except for get_committer(); I haven't gotten around to implementing User class which that requires.

--- pyvcal/git_wrapper/revision.py
+++ pyvcal/git_wrapper/revision.py
@@ -2,21 +2,21 @@ import time
 
 class Revision(object):
     def __init__(self, rev_id, repo):
-        # all we need is the hash; we let gitpython do the rest
+        # all we need is the hash and repo; we let gitpython do the rest
         super(Revision, self).__init__()
         self._rev_id = rev_id
         self._repo = repo
         self._git_commit = self._repo.commit(self._rev_id)
 
     def get_predecessors(self):
-        """Return a list of Revisions that flow into this Revision"""
-        # raise NotImplementedError im
+        """Return a list of immediate Revisions that flow into this Revision"""
         parent_list = self._git_commit.parents # git.Commit[]
         return list(Revision(p.id, p.repo) for p in parent_list)
         
     def get_properties(self):
         """Get the RevisionProperties for this revision."""
         # rev = self._repo.commit(self._rev_id) # git.Commit obj representing _rev_id
+        raise NotImplementedError
         author = User(self._git_commit.committer.name) # User(string)
         log = self._git_commit.message # string
         committed_timedate = self._git_commit.committed_date # python DateTime obj
@@ -29,15 +29,19 @@ class Revision(object):
         """
         # note to self: r1.diff(commit, commit.parents[0])
         diff = self._repo.diff(self._git_commit, self._git_commit.parents[0])
-        # TODO: return RevisionDiff(diff)
+        # TODO: implement RevisionDiff to implement this
         raise NotImplementedError
+    
+
         
     @classmethod
     def diff(cls, src, dst, paths=None):
         """Return the RevisionDiff from Revision src to Revision dst, optionally restricted to the given file(s) on paths"""
+        # paths functionality not implemented yet
+        # TODO: implement RevisionDiff to implement this
+        
         raise NotImplementedError
     
     predecessors = property(get_predecessors)
     properties = property(get_properties)
     diff_with_parent = property(get_diff_with_parent)
-
--- pyvcal/git_wrapper/revisionproperties.py
+++ pyvcal/git_wrapper/revisionproperties.py
@@ -1,27 +1,34 @@
 class RevisionProperties(object):
     """Metadata for a revision"""
-    def __init__(self):
+    def __init__(self, identity, repo):
         super(RevisionProperties, self).__init__()
+        self._id = identity # should be a 40char git commit hash id
+        self._repo = repo # repo this belongs to
     
     def get_revision(self):
-        """Return the revision to which these properties apply."""
-        pass
+        """Return the Revision obj to which these properties apply."""
+        return Revision(self._id, self._repo)
         
     def get_commit_message(self):
         """Return a unicode string containing the commit message for the Revision."""
-        pass
+        # return self._log
+        return self._repo.commit(self._id).message
+
         
     def get_committer(self):
         """Get the User who committed the Revision"""
-        pass
+        # TODO: Implement User class
+        return User(self._repo.commit(self._id).committer.name)
+        raise NotImplementedError
         
     def get_time(self):
         """Return the (TODO dataformat) of the revision"""
-        pass
+        # currently returns a python timedate obj
+        return self._repo.commit(self._id).committed_date
     
     def get_revision_id(self):
         """Return the revision identifier of the revision."""
-        pass
+        return self._id
     
     revision = property(get_revision)
     commit_message = property(get_commit_message)
