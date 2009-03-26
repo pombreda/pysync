ec8819d5b833e86844340420c9fff8df113aac34
veronica.quinones <veronica.quinones@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-03-26 16:53:28 +0000

    updating the skeleton of the revisiondiff class in subversion to match the new revisiondiff in pyvcal

--- pyvcal/subversion/revisiondiff.py
+++ pyvcal/subversion/revisiondiff.py
@@ -1,5 +1,3 @@
-from filediff import FileDiff
-
 import os
 
 from subvertpy import repos, ra, NODE_DIR, NODE_FILE
@@ -13,28 +11,13 @@ class RevisionDiff(object):
         self._rev1 = revision1
         self._rev2 = revision2
         
+    def get_value():
+        """Concatenation of Unified Diffs of resources between the revisions."""
+        raise NotImplementedError 
+
     def get_diff():
         resource1 = self._rev1.get_resource()
         resource2 = self._rev2.get_resource()
         
-        if resource1.is_file() and resource2.is_file(): 
-            return FileDiff(resource1, resource2)
-            
-
-class NullRevisionDiff(RevisionDiff):
-    """ A fake diff """
-
-    def __init__(self, null_rev=None):
-        super(NullRevisionDiff, self).__init__()
-        
-        self.null_rev = null_rev
-        
-
-class MultipleParentNullRevisionDiff(NullRevisionDiff):
-    """ A fake diff for when a revision doesn't have a unique parent """
-
-    def __init__(self, null_rev=None):
-        super(MultipleParentNullRevisionDiff, self).__init__()
-        
-        self.null_rev = NullRevisionDiff(null_rev)
+    value = property(get_value)
 
