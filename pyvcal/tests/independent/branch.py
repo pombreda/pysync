ef262233369984f08a535db9da7c6db5862bf90e
aran.donohue <aran.donohue@146e5f18-e271-11dd-a8eb-ddf07f3c1838>
2009-04-06 15:59:53 +0000

    Independent branch test: Avoid using private interface, rename reference to main branch in repository.

--- pyvcal/tests/independent/branch.py
+++ pyvcal/tests/independent/branch.py
@@ -7,7 +7,7 @@ class TestBranch(modulespecific.ModuleSpecificTestCase):
         """Create, connect to a repository and grab a branch."""
         self.basic_repo = self.test_module.BasicRepository()
         self.repo = self.basic_repo.repo()
-        self.branches = self.repo.branches['']
+        self.main_branch = self.repo.branches['']
         
     def tearDown(self):
         """Destroy the created repository."""
@@ -18,13 +18,13 @@ class TestBranchName(TestBranch):
     def runTest(self):
         """Test that the 'basic' test Branch.get_name returns the proper name"""
         self.assertTrue(self.branches)
-        name = self.branches.name
+        name = self.main_branch.name
         self.assertEquals(name, '')
 
 class TestBranchHead(TestBranch):
     """Test Branch.head"""
     def runTest(self):
         """Test that the 'basic' test Branch reports the latest revision in this branch."""
-        latest_rev = self.branches.head._get_revision_number()
-        self.assertEquals(latest_rev, 4)
+        latest_rev = self.main_branch.head.properties.revision_id
+        self.assertTrue(latest_rev)
         
